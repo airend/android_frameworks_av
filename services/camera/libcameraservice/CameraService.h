d9f9cc07e719c7ef58e47e703c7b64e28ff65a08
Jonas Lai <jonas.lai@mediatek.com>
2017-07-10 21:04:44 +0800

    CameraService: fix torch mode status logic
    The status of the torch mode of camera HAL and the framework may be
    inconsistant. It results from that the framework forces to update
    the status without receiving any notification from HAL, even if camera
    HAL supports the callback of the torch mode status.
    
    Fix the logic as below:
    a. A previously-disconnected camera device becomes connected, or
       a previously-connected camera becomes disconnected:
       In these cases, the framework will assume the default torch mode
       status according to the spec. Camera HAL does not need to invoke
       any torch mode callback.
       Thus, keep the original updates from the following path:
        * CameraService::onDeviceStatusChanged() -> updateStatus()
    
    b. Open or close a camera device:
       Camera HAL is responsible for updating the torch mode status by
       invoking a torch mode callback. The framework should not update
       the torch mode status without any notification from HAL.
       Thus, the updates of torch mode status from the following path
       must be disabled:
        * CameraService::BasicClient::startCameraOps() -> updateStatus()
        * CameraService::BasicClient::finishCameraOps() -> updateStatus()
    
    Test: manual testing
    Change-Id: Iee0d10371a41f6609139efa54336e2507496ec53
    Signed-off-by: D. Andrei Măceș <dmaces@nd.edu>

--- services/camera/libcameraservice/CameraService.h
+++ services/camera/libcameraservice/CameraService.h
@@ -699,10 +699,12 @@ private:
      */
     void updateStatus(StatusInternal status,
             const String8& cameraId,
+            bool updateTorchStatus,
             std::initializer_list<StatusInternal>
                 rejectedSourceStates);
     void updateStatus(StatusInternal status,
-            const String8& cameraId);
+            const String8& cameraId,
+            bool updateTorchStatus);
 
     // flashlight control
     sp<CameraFlashlight> mFlashlight;
